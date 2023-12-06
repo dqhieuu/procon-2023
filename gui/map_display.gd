16a9adbbfe9d4e88abd77c7dc4631d37d65e5512
Quang Hieu Dao <hieu_dq@flinters.vn>
2023-12-06 22:57:36 +0700

    Add showing max turn to gui

--- gui/map_display.gd
+++ gui/map_display.gd
@@ -178,7 +178,7 @@ func load_map(json):
 	
 	
 	for e in get_nodes_by_group("turn_info"):
-		e.text = "Turn %s: %s" % [game_state.turn_number, _turn_state_to_string(game_state.turn_state)]
+		e.text = "Turn %d/%d: %s" % [game_state.turn_number, json.options.max_turns, _turn_state_to_string(game_state.turn_state)]
 	
 
 	var replay_node = get_tree().get_first_node_in_group('replay_feature')
